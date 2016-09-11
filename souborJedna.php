fbdb5d8d2f5151c86bcd69f58993b830511b4579
tralisek <tralintralin@gmail.com>
2016-09-11 08:45:48 +0200

    Update souborJedna.php

--- souborJedna.php
+++ souborJedna.php
@@ -1,1 +1,29 @@
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "objednavky";
+
+// pripojeni k databazy
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// kontrola
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "insert into person (id, jmeno, prijmeni, pozice)
+	    values (1, 'honza', 'malasek', 'programator')";
+
+if ($conn->query($sql) === TRUE) {
+	echo "New record created succesfully";
+	}
+		else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		$conn->close();
+	
+	
+	
+	
+?>
